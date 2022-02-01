56114d5047969eebd4e72120bc7877a83e0303d7
Saurav kumar Srivastava <9s8k7s@gmail.com>
2022-02-01 17:25:27 +0530

    initial commit

---
+++ app/src/main/java/com/skdevstudio/encrypto/DBHelper.kt
@@ -0,0 +1,17 @@
+package com.skdevstudio.encrypto
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(context: Context) : SQLiteOpenHelper(context,"USERDB",null,1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE USERDATA(ID INTEGER PRIMARY KEY AUTOINCREMENT,ACCOUNT_TYPE TEXT,USERNAME TEXT,USER_PASSWORD TEXT)")
+        db?.execSQL("CREATE TABLE KEYS(KEY_ID INTEGER PRIMARY KEY AUTOINCREMENT,SECRET_SECONDARY_KEY TEXT)")
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+}
